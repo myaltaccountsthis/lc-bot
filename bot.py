00991c04a5306e0e4fd8e64ff4e60f6a61fe656f
Sahasrad Chippa <52544077+MetaFlash@users.noreply.github.com>
2024-10-18 17:24:42 -0400

    Update commenting and query test formatting

--- bot.py
+++ bot.py
@@ -11,16 +11,20 @@ tree = discord.app_commands.CommandTree(bot)
 async def on_ready():
     print(f"Logged in as {bot.user}")
     try:
+        # Sync slash command tree
         synced = await tree.sync()
         print(f"Synced {len(synced)} commands")
     except Exception as e:
         print(f"Failed to sync commands: {e}")
 
-@tree.command(name="ping", description="Display current latency")
+@tree.command(name="ping", description="Display latency information")
 async def ping(interaction: discord.Interaction):
     text = f'Pong! {round (bot.latency * 1000)} ms initial.'
+    # Initial ping message
     await interaction.response.send_message(text)
     msg = await interaction.original_response()
+
+    # Edit the message with the latency of original response
     time = round((msg.created_at - interaction.created_at).total_seconds() * 1000)
     await msg.edit(content=text + f' {time} ms round-trip.')
 
--- query.py
+++ query.py
@@ -5,19 +5,23 @@ from gql.transport.aiohttp import AIOHTTPTransport
 transport = AIOHTTPTransport(url="https://leetcode.com/graphql")
 client = Client(transport=transport, execute_timeout=60)
 
-def readFile(fileName):
-    with open(fileName, "r") as file:
-        content = file.read()
+def read_file(file_name):
+    with open(file_name, "r") as f:
+        content = f.read()
     return content
 
 queries = {}
 QUERIES_PATH = "queries/"
+OUT_PATH = "out/"
+os.makedirs(OUT_PATH, exist_ok=True)
+
 for file in os.listdir(QUERIES_PATH):
-    queries[file[:file.find(".gql")]] = gql(readFile(QUERIES_PATH + file))
+    queries[file[:file.find(".gql")]] = gql(read_file(QUERIES_PATH + file))
 
-def doQuery(queryName, values = {}):
-    client.execute(queries[queryName], variable_values=values)
+def do_query(query_name, values = {}):
+    return client.execute(queries[query_name], variable_values=values)
 
-result = doQuery("problemsetQuestionList", values={"categorySlug": "", "skip": 0, "limit": 50, "filters": {}})
-# with open("out/questionlistshort.txt", "w") as file:
-#     file.write(str(result))
+limit = 50 # -1 if no limit
+result = do_query("problemsetQuestionList", values={"categorySlug": "", "skip": 0, "limit": limit, "filters": {}})
+with open(OUT_PATH + "questionList.txt", "w") as file:
+    file.write(str(result))
