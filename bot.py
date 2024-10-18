e22e27a3bcbcf04862314f99c0617f4a3f657311
Sahasrad Chippa <52544077+MetaFlash@users.noreply.github.com>
2024-10-18 16:58:04 -0400

    Initialize Discord.py Bot
    Slash commands tree and ping command

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
-print("so true")
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+load_dotenv()
+
+bot = discord.Client(intents=discord.Intents.default())
+tree = discord.app_commands.CommandTree(bot)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    try:
+        synced = await tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(f"Failed to sync commands: {e}")
+
+@tree.command(name="ping", description="Display current latency")
+async def ping(interaction: discord.Interaction):
+    text = f'Pong! {round (bot.latency * 1000)} ms initial.'
+    await interaction.response.send_message(text)
+    msg = await interaction.original_response()
+    time = round((msg.created_at - interaction.created_at).total_seconds() * 1000)
+    await msg.edit(content=text + f' {time} ms round-trip.')
+
+bot.run(token=os.getenv('BOT_TOKEN'))
